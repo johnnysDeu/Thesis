ca8bd3f5167a352cbcb7e20f5af532f5fd12a5c4
johnnysDeu <doitsinisgiannis@hotmail.com>
2024-04-03 11:05:20 +0200

    identify_image_color 2

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import Find_duplicates
 import display_images
 import time
 
+import functions
 
 # when delete flag = true, delete the duplicate
 # run this for all folders
@@ -10,19 +11,27 @@ delete_flag = False
 start_time = time.time()
 if __name__ == "__main__":
     # Current_dir = os.getcwd()
-    Current_dir = "C:\\Users\\YannisPC\\PycharmProjects\\Thesis\\Thesis\\Crawler_results_Germany"
+    #Current_dir = "C:\\Users\\YannisPC\\PycharmProjects\\Thesis\\Thesis\\Crawler_results_Germany"
+    Current_dir = "C:\\Users\\doitsinis\\PycharmProjects\\Thesis\\Crawler_results_Germany"
+
     subfolders = [f.path for f in os.scandir(Current_dir) if f.is_dir()]
-    # print(subfolders)
+    #print(subfolders)
 
     for fold in list(subfolders):
         files = os.listdir(fold)
         print("Current folder : ", fold)
         # call for all folders in Germany
         #Find_duplicates.find_complete_duplicate_images(fold, delete_flag)
-        Find_duplicates.find_near_duplicates(fold,delete_flag)
+        #Find_duplicates.find_near_duplicates(fold,delete_flag)
         #images_data = read_from_db(fold)
         #print(images_data)
         #print(os.getcwd())
+
+
+        # identifying all black, all white or all same color img
+        functions.identify_image_color(fold)
+        #print(result)
+
 print("--- %s seconds ---" % (time.time() - start_time))
 
 # when delete flag = true, delete the duplicate
@@ -30,11 +39,11 @@ delete_flag = False
 # run this for specific folder
 start_time = time.time()
 if __name__ == "__main__":
-    #folder_path = "C:\\Users\\doitsinis\\PycharmProjects\\Thesis\\folder_108"# douleia
-    folder_path = "C:\\Users\\YannisPC\\PycharmProjects\\Thesis\\Thesis\\Crawler_results_Germany\\folder_1" #spiti
+    folder_path = "C:\\Users\\doitsinis\\PycharmProjects\\Thesis\\folder_108"# douleia
+    #folder_path = "C:\\Users\\YannisPC\\PycharmProjects\\Thesis\\Thesis\\Crawler_results_Germany\\folder_1" #spiti
     #print("Current folder : ", fold)
     #print("Current folder : ", folder_path)
-    folder_name = os.path.split(folder_path)
+    #folder_name = os.path.split(folder_path)
     #print(folder_name)
     #Find_duplicates.find_complete_duplicate_images(folder_path)
     #Find_duplicates.find_near_duplicates(folder_path, delete_flag)
@@ -44,10 +53,14 @@ print("--- %s seconds ---" % (time.time() - start_time))
 
 # black and white image, testing
 if __name__ == "__main__":
-    image_path = "C:\\Users\\doitsinis\\PycharmProjects\\Thesis\\folder_108\\iframe_72.png"
+    image_path = "C:\\Users\\doitsinis\\PycharmProjects\\Thesis\\folder_108\\iframe_101.jpg"
     folder_path = "C:\\Users\\doitsinis\\PycharmProjects\\Thesis\\folder_108"
     #functions.img_is_black_or_white(folder_path)
     #result=functions.img_is_black_or_white(image_path)
+
+    #result = functions.identify_image_color(image_path)
+    #print(result)
+
     #if result:
     #    print(f"The image at '{image_path}' is either completely white or black.")
     #else:
